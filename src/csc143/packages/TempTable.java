e89a391d1468f276ff67e4b0e0cdaaed2daf5377
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-07-06 01:16:30 -0700

    added files for LA3

---
+++ src/csc143/packages/TempTable.java
@@ -0,0 +1,21 @@
+package csc143.packages;
+
+/**
+ * A simple class that uses classes in named packages.
+ */
+public class TempTable {
+
+   /**
+    * A program that prints out a temperature conversion table
+    * @param args The command-line arguments
+    */
+   public static void main(String[] args) {
+   
+      // print out headers
+      System.out.println("Celsius  Fahrenheit");
+      
+      // print out values
+      
+   }
+   
+}
---
+++ src/csc143/packages/temp/Converter.java
@@ -0,0 +1,19 @@
+package csc143.packages.temp;
+
+/**
+ * A simple supplier class that converts temperature values.
+ */
+public class Converter {
+
+   /**
+    * Converts Celsius to Fahrenheit.
+    * @param value The Celsius temperature to be converted
+    * @return      The calculated Fahrenheit temperature
+    */
+   public static double c2f(double value) {
+   
+      return 0;
+      
+   }
+   
+}
