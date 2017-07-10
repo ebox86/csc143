35a2164bb8f2dff8e694fd0991505fb838ae0ae6
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-07-10 02:10:19 -0700

    finished commenting hw2 - sudoku board

--- src/csc143/sudoku/SudokuBase.java
+++ src/csc143/sudoku/SudokuBase.java
@@ -1,43 +1,101 @@
 package csc143.sudoku;
 
+/**
+ * SudokuBase - abstract class that contains the basic methods 
+ * for what the Sudoku board can do
+ * @author Evan Kohout
+ * @version Assignment 2: Sudoku Board
+ */
 public abstract class SudokuBase extends java.util.Observable {
     
+	// the rows of the sukoku board
     private final int rows;
+    // the columns of the sudoku board
     private final int columns;
+    // the size of the board in terms of number of regions
     private final int size;
     
+    /**
+     * State -- public enumeration for returning various states of the board
+     *
+     */
     public enum State {COMPLETE, INCOMPLETE, ERROR};
-    
+
+    /**
+     * SudokuBase -- constructs a new SudokuBase object
+     * @param layoutRows -- number of rows for the game board
+     * @param layoutColumns -- number of columns for the game board
+     */
     public SudokuBase(int layoutRows, int layoutColumns) {
         rows = layoutRows;
         columns = layoutColumns;
         size = columns * rows;
     }
     
+    /**
+     * getRows -- getter method used to return the number of rows in the game board
+     * @return -- number of rows
+     */
     public int getRows() {
         return rows;
     }
     
+    /**
+     * getColumns -- getter method used to return the number of columns in the game board
+     * @return -- number of columns
+     */
     public int getColumns() {
         return columns;
     }
     
+    /**
+     * getSize -- getter method used to return the size of the game board
+     * @return -- an integer value of the size of the game board
+     */
     public int getSize() {
         return size;
     }
     
     public abstract int getValue(int row, int col);
     
+    /**
+     * setValue -- setter method used to assign value to a particular cell
+     * @param row -- the row of the cell to be assigned a value
+     * @param col -- the column of the cell to be assigned a value
+     * @param value -- the value to be assigned
+     */
     public abstract void setValue(int row, int col, int value);
     
+    /**
+     * isGiven -- checks to see whether a certain cell on the board is a given value or not
+     * @param row -- the row of the cell to be checked
+     * @param col -- the column of the cell to be checked
+     * @return -- boolean
+     */
     public abstract boolean isGiven(int row, int col);
+    
     
     public abstract void fixGivens();
     
+    /**
+     * getRowState -- the current state of a given row n
+     * @param n
+     * @return State -- the current state of the row n
+     */
     public abstract State getRowState(int n);
     
+    /**
+     * getColumnState -- the current state of a given column n
+     * @param n
+     * @return State --  the current state of the column n
+     */
     public abstract State getColumnState(int n);
     
+    /**
+     * getRegionState -- the current state of a given region n
+     * @param n
+     * @return State -- the current state of the region n
+     */
     public abstract State getRegionState(int n);
     
 }
--- src/csc143/sudoku/SudokuBoard.java
+++ src/csc143/sudoku/SudokuBoard.java
@@ -8,25 +8,47 @@ import java.util.List;
 
 import javax.swing.JPanel;
 
+/**
+ * Sudoku Board -- creates and displays a graphical sudoku board
+ * @author Evan Kohout
+ * @version Assignment 2: Sudoku Board
+ */
 public class SudokuBoard extends javax.swing.JPanel {
     
-	SudokuBase b;
-	int cols;
-	int rows;
-	int cellSize = 50;
-	int cellPadding = 2;
+	// ~~ fields ~~ //
 	
+	SudokuBase b;
+	private final int cols;
+	private final int rows;
+	private final int cellSize = 50;
+	private final int cellPadding = 2;
+	
+	// ~~ constructor ~~ //
+	
+	/**
+	 * SudokuBoard -- constructs the sudokuBoard object
+	 * @param b -- the sudoku base object
+	 */
     public SudokuBoard(SudokuBase b) {
     	cols = b.getColumns();
     	rows = b.getRows();
     	this.b = b;
     }
     
+    // ~~ methods ~~ //
     
+    /**
+     * getBase -- implements abstract class getBase from SudokuBase
+     * @return b -- the board
+     */
     SudokuBase getBase() {
     	return this.b;
     }
     
+    /**
+     * paintComponent -- overrides JPanel's paintComponent method and 
+     * displays graphical components to the frame
+     */
     public void paintComponent(Graphics g){
     	super.paintComponent(g);
 		int xVal = 1;
@@ -71,12 +93,15 @@ public class SudokuBoard extends javax.swing.JPanel {
     		xVal = cellPadding - 1;
 			yVal += (cellSize) * rows;
     	}
-    	// draws outer most border
+    	// draws outer most black border
     	g.setColor(Color.black);
     	g.drawRect(0, 0, (cellSize) * (cols * rows) + cellPadding + 1, (cellSize) * (cols * rows) + cellPadding + 1);
     }
     
 
+    /*
+     * creates a new 'region' on the sudoku board
+     */
     private void drawRegion(Graphics g, int xVar, int yVar, Color c){
     	g.setColor(c);
 		g.fillRect(xVar + 1, yVar + 1, cellSize * cols, cellSize * rows);
@@ -87,17 +112,28 @@ public class SudokuBoard extends javax.swing.JPanel {
 		}
     }
     
+    /*
+     * creates a new cell within a region of the sudoku board
+     * offset by 1 pixel in both x and y axis so to create the 
+     * 1 px border with the region background color
+     */
     private void drawCell(Graphics g, int x, int y, int side){
     	g.setColor(Color.black);
     	g.drawRect(x + 1, y + 1, side, side);
     }
     
+    /*
+     * resizes the frame to the appropriate height and width per the board size
+     */
     @Override
     public Dimension getPreferredSize() {
        return new Dimension(b.getColumns() * b.getRows() * cellSize + 4, b.getRows() * b.getColumns() * cellSize + 4);
     }
     
     
+    /*
+     * Test application method which tests the frame and creates a 2x3 sudoku board
+     */
     public static void main(String[] args) {
         javax.swing.JFrame win = new javax.swing.JFrame("Test 2x3");
         win.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
