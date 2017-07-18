d1f9d591ad05a27f9496d3a7e30d289c22f01352
Kohout <evankoh@amazon.com>
2017-07-17 19:11:19 -0700

    finished HW3 to minimum spec, added comments

--- src/csc143/sudoku/SudokuModel.java
+++ src/csc143/sudoku/SudokuModel.java
@@ -5,68 +5,65 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import csc143.sudoku.SudokuBase.State;
 import csc143.sudoku.SudokuCore;
-
+/**
+ * Model used to determine the state of rows, columns and regions on the sudoku board
+ * @author Evan Kohout
+ * @version csc143 - Homework 3: Sudoku Main
+ *
+ */
 public class SudokuModel extends SudokuCore {
     
     public SudokuModel(int r, int c) {
         super(r, c);
     }
     
+    /**
+     * getRowState -- determines the state of a given row n
+     * @return State -- the state of the row
+     */
     public State getRowState(int n) { 
     	List check = new ArrayList();
         for(int i = 0; i < getSize(); i++){
         	if(getValue(n, i) != 0) check.add(getValue(n, i));
         }
-        Set<Integer> set = new HashSet<Integer>(check);
-    	
-        if(set.size() < check.size()){
-            return State.ERROR;
-        } else if(check.size() <= getSize()){
-        	return State.INCOMPLETE;
-        } else {
-        	return State.COMPLETE;
-        }
+    	return stateHelper(check);
     }
     
+    /**
+     * getColumnState -- determines the state of a given column n
+     * @return State -- the state of the column
+     */
     public State getColumnState(int n) { 
     	List check = new ArrayList();
         for(int i = 0; i < getSize(); i++){
         	if(getValue(i, n) != 0) check.add(getValue(i, n));
         }
-        Set<Integer> set = new HashSet<Integer>(check);
-    	
-        if(set.size() < check.size()){
-            return State.ERROR;
-        } else if(check.size() <= getSize()){
-        	return State.INCOMPLETE;
-        } else {
-        	return State.COMPLETE;
-        }
+    	return stateHelper(check);
     }
     
+    /**
+     * getRegionState -- determines the state of a given region n
+     * @return State -- the state of the region
+     */
     public State getRegionState(int n) { 
-    	List check = new ArrayList();
-		int j, k = 0;
-    	for(int i = 0; i <= n; i++, j += getColumns(), k += getRows()){
-    		check.clear();
-    		for(j = 0; j < getRows(); j++){
-    			for(k = 0; k < getColumns(); k++){
-    				if(getValue(k, j) != 0) check.add(getValue(k, j));
-    			}
-    		}
-    	}
-    	Set<Integer> set = new HashSet<Integer>(check);
-    	for(Object o : check) System.out.println(o);
-        if(set.size() < check.size()){
-            return State.ERROR;
-        } else if(check.size() <= getSize()){
-        	return State.INCOMPLETE;
-        } else {
-        	return State.COMPLETE;
-        }
+
+    	// incomplete
+    	return State.ERROR;
         
     }
     
+    /*
+     * helper method used to determine the state after collecting a list of cell values
+     */
+    private State stateHelper(List l){
+    	Set<Integer> set = new HashSet<Integer>(l);
+        if(set.size() < l.size()){
+            return State.ERROR;
+        } else if(l.size() <= getSize()){
+        	return State.INCOMPLETE;
+        } else {
+        	return State.COMPLETE;
+        }
+    }
 }
