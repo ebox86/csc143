0b6e52e164ace6b0e117c1443fdcd0bf2a041327
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-08-07 00:48:41 -0700

    added class for HW6

---
+++ src/csc143/data_structures/MyTreeMap.java
@@ -0,0 +1,38 @@
+package csc143.data_structures;
+
+public class MyTreeMap implements BasicMap<K, V> {
+
+		
+	public MyTreeMap() {
+		
+	}
+
+	@Override
+	public void put(K key, V value) {
+		
+		
+	}
+
+	@Override
+	public V get(Object key) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		
+		
+	}
+
+}
+
+class Node {
+	
+}
